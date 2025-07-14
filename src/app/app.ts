6fc03a49e15d3ab1289187fee652043f940761e8
PUPUENDO <pupuendo12@gmail.com>
2025-07-14 02:01:57 -0600

    fix: create more components

--- src/app/app.ts
+++ src/app/app.ts
@@ -12,13 +12,19 @@ import { CardOverview } from './card-overview/card-overview';
 import { ButtonToggleSelectionMode } from './button-toggle-selection-mode/button-toggle-selection-mode';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material/button';
+import { SlideToggleWithForms } from './slide-toggle-with-forms/slide-toggle-with-forms';
+import { SortingOverview } from './sorting-overview/sorting-overview';
+import { StepperVertical } from './stepper-vertical/stepper-vertical';
+import { TabGroup } from './tab-group/tab-group';
+import { TableExpandableRows } from './table-expandable-rows/table-expandable-rows';
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet, NgIf, MatAutocomplete, BadgeOverview, BasicCheckboxes, ExpansionPanel,
     SimpleFormField, ConfigurableProgressSpinner, ConfigurablePaginator, CardOverview, ButtonToggleSelectionMode,
-    MatMenuModule, MatButtonModule
+    MatMenuModule, MatButtonModule,
+    SlideToggleWithForms, SortingOverview, StepperVertical, TabGroup, TableExpandableRows
   ],
   templateUrl: './app.html',
   styleUrl: './app.css'
---
+++ src/app/tab-group/tab-group.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+import {MatTabsModule} from '@angular/material/tabs';
+
+@Component({
+  selector: 'app-tab-group',
+  imports: [MatTabsModule],
+  templateUrl: './tab-group.html',
+  styleUrl: './tab-group.css'
+})
+export class TabGroup {
+
+}
